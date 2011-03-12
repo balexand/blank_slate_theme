7c7f4ebe75e14da0e2e5754684cb3a01d0843512
Brian Alexander <balexand@gmail.com>
2011-03-12 20:41:04 +0700

    clean up whitespace

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once("layout.php");
 
 layout(function() { ?>
-  
+
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
     <?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?>
@@ -11,9 +11,9 @@ layout(function() { ?>
   <?php endwhile; else: ?>
     <p>Sorry, no posts matched your criteria.</p>
   <?php endif; ?>
-  
+
   <?php get_sidebar() ?>
-  
+
 <?php }); // end layout
 
 ?>
